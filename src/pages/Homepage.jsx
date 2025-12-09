9b650ca77ff9937b1b7f58dacc6ac325e3d4315c
Enoch Selasi Doe <enochdoe355@gmail.com>
2025-12-09 16:58:45 +0000

    fetched data from backend API and displayed in table

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import Button from "../components/ButtonComponent/Button";
 import Modal from "../components/Modal/Modal";
 
@@ -7,6 +8,30 @@ import "./Homepage.css";
 function Homepage() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [employees, setEmployees] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const res = await fetch("http://localhost:5000/employees");
+        if (!res.ok) throw Error("Failed to load employees");
+
+        const data = await res.json();
+        setEmployees(data);
+        console.log(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchEmployees();
+  }, []);
 
   return (
     <div className="section">
@@ -23,49 +48,67 @@ function Homepage() {
       <Modal openModal={isOpen} closeModal={() => setIsOpen(false)} />
       <br />
       <div className="table">
-        <table>
-          <thead>
-            <tr>
-              <th className="no-column">No.</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Salary</th>
-              <th>Date</th>
-              <th className="actions-column">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Susan</td>
-              <td>Jordon</td>
-              <td>susan@example.com</td>
-              <td>$95,000</td>
-              <td>2019-04-11</td>
-              <td>
-                <div class="actions">
-                  <Button
-                    variant="secodary"
-                    size="sm"
-                    className="table-btn
-                    "
-                  >
-                    Edit
-                  </Button>
-                  <Button
-                    variant="secodary"
-                    size="sm"
-                    className="table-btn
-                    "
-                  >
-                    Delete
-                  </Button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        {isLoading && (
+          <div className="loading-icon-container">
+            <CircularProgress />
+          </div>
+        )}
+        {error && (
+          <div className="error-text-container">
+            <p>{error}</p>
+          </div>
+        )}
+        {!isLoading && !error && (
+          <table>
+            <thead>
+              <tr>
+                <th className="no-column">No.</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Salary</th>
+                <th>Date</th>
+                <th className="actions-column">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.length === 0 ? (
+                <tr>
+                  <td>No employees</td>
+                </tr>
+              ) : (
+                employees.map((emp, index) => (
+                  <tr key={emp._id}>
+                    <td>{index + 1}</td>
+                    <td>{emp.firstName}</td>
+                    <td>{emp.lastName}</td>
+                    <td>{emp.email}</td>
+                    <td>{emp.salary}</td>
+                    <td>{new Date(emp.createdAt).toLocaleDateString()}</td>
+                    <td>
+                      <div className="actions">
+                        <Button
+                          variant="secondary"
+                          size="sm"
+                          className="table-btn"
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          variant="secondary"
+                          size="sm"
+                          className="table-btn"
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
